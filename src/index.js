96f8b6b7be38b01beef3a3f265af35dd0cf38e5a
tomboo <tom.booster@gmail.com>
2019-12-28 16:48:53 -0800

    Refactor src/App.js file into multiple component modules

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import 'font-awesome/css/font-awesome.min.css';
-import App from './App';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
 // Put any other imports below so that CSS from your
